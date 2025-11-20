2d5db095c06156e5d630f3069ec201750f58fca8
shellshock0x221B <shellshock0x1@proton.me>
2025-11-20 20:57:30 +0100

    Update elements.h

--- elements.h
+++ elements.h
@@ -5,7 +5,6 @@
 
 #define G 1.0f
 
-
 typedef struct {
     Vector3 pos;
     Vector3 vel;
@@ -13,22 +12,25 @@ typedef struct {
     Color col;
     float radius;
     Vector3 acc;
-
 } Object;
 
-
-
 extern Object sun;
-extern Object planet;
+extern Object mercury;
+extern Object venus;
 extern Object earth;
-extern Object planet1;
-
+extern Object mars;
+extern Object jupiter;
+extern Object saturn;
+extern Object uranus;
+extern Object neptune;
+extern Object moon;
 
 void createObjects(void);
 void drawObjects(void);
 void updatePhysics(void);
-void DrawWarpedGrid(Object sun, Object planet, Object earth , Object planet1);
+void DrawWarpedGrid(Object sun, Object mercury, Object venus, Object earth,
+                    Object mars, Object jupiter, Object saturn,
+                    Object uranus, Object neptune);
 float warpEffect(Vector3 point, Object obj);
-
 
 #endif // ELEMENTS_H
